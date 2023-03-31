3f87af7cee0d08a2ba628f7a6383774affdba8b4
tan-iC <okaz2829515wotaku@gmail.com>
2023-03-31 21:54:10 +0900

    delete some scripts

--- scripts/criterion/__init__.py
+++ scripts/criterion/__init__.py
@@ -1,29 +1,3 @@
 # from .${fileName} import ${className}
-from .ratio \
-    import MultiRefRatioLoss
-from .diff \
-    import MultiRefDiffLoss
-from .diffVal \
-    import MultiRefDiffValLoss
-from .ratioVal \
-    import MultiRefRatioValLoss
-from .logRatio \
-    import MultiRefLogRatioLoss
-from .diffMax \
-    import MultiRefDiffMaxLoss
-from .diffValPlus \
-    import MultiRefDiffValPlusLoss
-from .diffValPlusSquared \
-    import MultiRefDiffValPlusSquaredLoss
-from .orderPenalty \
-    import MultiRefOrderPenaltyLoss
-from .diffValPlusSqrt \
-    import MultiRefDiffValPlusSqrtLoss
 from .diffMaxSquared \
     import MultiRefDiffMaxSquaredLoss
-from .diffPlusSqrtSquared \
-    import MultiRefDiffPlusSqrtSquaredLoss
-from .diffCompSquaredMax \
-    import MultiRefDiffCompSquaredMaxLoss
-from .diffRatioSquaredMax \
-    import MultiRefDiffRatioSquaredMaxLoss
