cfd6445a8218ee04e33f7c09d8ac86d8a62fcc0e
tan-iC <okaz2829515wotaku@gmail.com>
2023-04-01 16:18:01 +0900

    clean and split

--- scripts/criterion/diffMaxSquared.py
+++ scripts/criterion/diffMaxSquared.py
@@ -6,6 +6,7 @@
 import math
 from dataclasses import dataclass
 from unittest.main import MAIN_EXAMPLES
+from .custom import extract_from_sample
 
 import torch.nn.functional as F
 from fairseq import metrics, utils
@@ -52,7 +53,7 @@ class MultiRefDiffMaxSquaredLoss(FairseqCriterion):
         # 学習時のみ分解する
         if model.training == True:
             additional, sample =\
-                self.extract_from_sample(
+                extract_from_sample(
                     sample, 
                     padding_idx=self.padding_idx, 
                     eos_idx=self.eos_idx,
@@ -160,200 +161,6 @@ class MultiRefDiffMaxSquaredLoss(FairseqCriterion):
         """
         return True
 
-    def eosSplit(self, target, padding_idx=1, eos_idx=2):
-        "参照文のトークン列targetをeosで分割する"
-        eos_flag = False
-        tmp = []
-        targets = []
-        for i, tkn in enumerate(target):
-
-            if (eos_flag == True) and (tkn != padding_idx):
-                targets.append(tmp)
-                tmp = []
-                eos_flag = False
-
-            tmp.append(tkn)
-
-            if (tkn == eos_idx) :
-                eos_flag = True
-
-            if (i == len(target)-1):
-                targets.append(tmp)
-                tmp = []
-
-        return torch.LongTensor(targets)
-
-    def listPadding(self, ref, tgt_len, padding_idx=1, eos_idx=2, prev=False):
-        "list::refをint::tgt_lenまでpadding"
-        refPadded = ref
-
-        while(len(refPadded) != tgt_len):
-            refPadded.append(padding_idx)
-
-        return refPadded
-
-    def sepSplit(self, targets, tgt_len=None, eos_idx=2, sep_idx=4, prev=False):
-        "参照文のトークン列のリストであるtargetsのそれぞれをsepで区切る"
-        tmp     = []
-        tgts    = []
-        tgt_set = []
-
-        if tgt_len == None:
-            tgt_len = targets.size(1)
-
-        # 難易度（LoD : Level_of_Difficulty）
-        LoDs    = []
-        LoDs_set= []
-
-        for target in targets:
-
-            for i, tkn in enumerate(target):
-                if (prev==True) and (tkn==eos_idx):
-                    continue
-
-                if (tkn == sep_idx) :
-                    if prev==False :
-                        tmp.append(eos_idx)
-
-                    # 難易度を取り出す
-                    LoDs.append(tmp[0])
-
-                    if prev==True :
-                        tmp.insert(1, eos_idx)
-
-                    tmp = self.listPadding(tmp[1:], tgt_len, prev=prev)
-                    tgts.append(tmp)
-
-                    tmp = []
-
-                else :
-                    tmp.append(tkn)
-
-
-                if (i == len(target)-1):
-
-                    # 難易度を取り出す
-                    LoDs.append(tmp[0])
-
-
-                    if prev==True :
-                        tmp.insert(1, eos_idx)
-
-                    tmp = self.listPadding(tmp[1:], tgt_len, prev=prev)
-                    tgts.append(tmp)
-
-                    tmp = []
-            
-            tgt_set.append(torch.LongTensor(tgts))
-            tgts = []
-
-            # 難易度
-            LoDs_set.append(torch.LongTensor(LoDs))
-            LoDs = []
-
-        return tgt_set, LoDs_set
-
-    def make_main_LoDs(self, net_inputs, padding_idx=1):
-        "mainの難易度を取得する"
-        main_LoDs = []
-
-        for net_input in net_inputs:
-
-            for tkn in net_input:
-                if tkn == padding_idx:
-                    continue
-
-                main_LoDs.append(tkn)
-                break
-
-        return torch.LongTensor(main_LoDs)
-
-    def get_max_length(self, tensors):
-        "tensorsの最大文長を求める"
-        tmp = []
-        for tensor in tensors:
-            for tokens in tensor:
-                tokens = tokens[torch.where(tokens > 1)]
-                tmp.append(len(tokens))
-
-        return max(tmp)
-
-    def output_padding(self, tensors, max_length, prev=False):
-        "最大文長に合わせる"
-        out = []
-        for tensor in tensors:
-            for tokens in tensor:
-                tokens = tokens[torch.where(tokens > 1)]
-                out.append(self.listPadding(tokens.tolist(), tgt_len=max_length, prev=prev))
-
-        return torch.LongTensor(out)
-
-    def extract_from_sample(self, sample, padding_idx=1, eos_idx=2, sep_idx=4):
-
-        # データの用意
-        src_tokens  = sample["net_input"]["src_tokens"]
-        prev_output = sample["net_input"]["prev_output_tokens"]
-        target      = sample["target"]
-
-        # mainの参照文の難易度を取得
-        main_LoDs = self.make_main_LoDs(src_tokens)
-
-        # sepトークンによる分割
-        targets, LoDs_set       = self.sepSplit(target)
-        prev_outputs, _LoDs_set = self.sepSplit(prev_output, prev=True)
-
-        # paddingを除く最大文長の取得
-        max_length = self.get_max_length(targets)
-
-        # 和集合の最大文長に合わせてpadding
-        targets         = self.output_padding(targets, max_length)
-        prev_outputs    = self.output_padding(prev_outputs, max_length, prev=True)
-
-        # mainとsubの分類
-        i = 0
-        sub_targets_set = []
-        sub_LoDs_set    = []
-        main_targets    = []
-        main_prev_output_tokens     = []
-        sub_prev_output_tokens_set  = []
-
-        for (LoDs, main_LoD) in zip(LoDs_set, main_LoDs):
-            tmp_prev    = []
-            tmp_targets = []
-            tmp_LoDs    = []
-
-            for LoD in LoDs:
-                if LoD == main_LoD:
-                    main_prev_output_tokens.append(prev_outputs[i].tolist())
-                    main_targets.append(targets[i].tolist())
-                
-                else:
-                    tmp_prev.append(prev_outputs[i].tolist())
-                    tmp_targets.append(targets[i].tolist())
-                    tmp_LoDs.append(LoD)
-                i += 1
-
-            sub_prev_output_tokens_set.append(torch.LongTensor(tmp_prev))
-            sub_targets_set.append(torch.LongTensor(tmp_targets))
-            sub_LoDs_set.append(torch.LongTensor(tmp_LoDs))
-
-        main_targets = torch.LongTensor(main_targets)
-        main_prev_output_tokens = torch.LongTensor(main_prev_output_tokens)
-
-        # loss計算時に利用する情報のディクショナリを作成
-        additional = {
-            "max_length": max_length,
-            "sub_targets_set": sub_targets_set,
-            "sub_prev_output_tokens_set": sub_prev_output_tokens_set,
-            "main_LoDs": main_LoDs,
-            "sub_LoDs_set": sub_LoDs_set,
-        }
-
-        # sampleの変数の更新
-        sample.update(target=main_targets)
-        sample["net_input"].update(prev_output_tokens=main_prev_output_tokens)
-
-        return additional, sample
 
     def my_loss_loop_01(
         self,
@@ -433,13 +240,6 @@ class MultiRefDiffMaxSquaredLoss(FairseqCriterion):
             # L_main + alpha * 1/n * sum(abs(d_i * L_main - L_sub)))
             ###
             tmp_loss = L_main + (alpha * (1 / sub_length) * sum_diff)
-
-            ###
-            # log
-            ###
-
-            # with open("/mntdir/tani/work/myFairseqScripts/repMultipleReferences/log/loss.log", mode="a") as f:
-            #     f.write(f'tmp_loss.grad_fn: {tmp_loss.grad_fn}')
 
             # 加算
             if loss == None:
